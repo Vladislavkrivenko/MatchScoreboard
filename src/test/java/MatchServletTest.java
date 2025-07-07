f3e63f48fc91f34e5f94eb5efef8b57d5310c138
Vladislavkrivenko <vladislavkrivenko7@gmail.com>
2025-07-07 15:04:02 +0300

    feat: создал пакеты, README, дополнил пом файл

---
+++ src/test/java/MatchServletTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MatchServletTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true, "Test setup works!");
+    }
+}
